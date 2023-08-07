a5a334c936c494f83ae5e0536bf7b3d3ed5f68cd
jrdrwn <jordirwn@gmail.com>
2023-08-07 14:30:50 +0700

    add responsive for homepage

--- src/components/shared/header/index.jsx
+++ src/components/shared/header/index.jsx
@@ -1,87 +1,89 @@
 /* eslint-disable react-refresh/only-export-components */
-import "./style.css";
-import { Link } from "react-router-dom";
-import jsCookie from "js-cookie";
-import { MdArrowDropDown } from "react-icons/md";
-import Cookies from "js-cookie";
-import { useEffect, useState } from "react";
-import { useFetch } from "use-http";
-import brand1 from "./brand-1.svg";
-import brand2 from "./brand-2.svg";
+import { default as Cookies, default as jsCookie } from 'js-cookie'
+import { useEffect, useState } from 'react'
+import { MdArrowDropDown, MdClose, MdMenu } from 'react-icons/md'
+import { Link } from 'react-router-dom'
+import { useFetch } from 'use-http'
+import brand1 from './brand-1.svg'
+import brand2 from './brand-2.svg'
+import './style.css'
 
 function Header({ navs, leftAddon, rightAddon, id }) {
+  const [nav, setNav] = useState(false)
+
   return (
     <header id={id}>
       {leftAddon}
-      <div>
+      <div id="nav-desktop">
         <nav>
           {navs.map((nav, index) => {
             return (
               <a href={nav.path} key={index}>
                 {nav.name}
               </a>
-            );
+            )
+          })}
+        </nav>
+        <div>{rightAddon}</div>
+      </div>
+      <button className="btn-mobile" data-nav={nav} onClick={() => setNav(!nav)}>
+        {nav ? <MdClose size={30} /> : <MdMenu size={30} />}
+      </button>
+      <div id="nav-mobile">
+        <nav>
+          {navs.map((nav, index) => {
+            return (
+              <a href={nav.path} key={index}>
+                {nav.name}
+              </a>
+            )
           })}
         </nav>
         <div>{rightAddon}</div>
       </div>
     </header>
-  );
+  )
 }
 
 function Primary({ leftAddon, rightAddon }) {
   const navs = [
-    { name: "Beranda", path: "#" },
-    { name: "Tentang", path: "#about" },
-    { name: "Layanan", path: "#services" },
-    { name: "Tim", path: "#team" },
-    { name: "Kontak", path: "#contact" },
-  ];
-  return (
-    <Header
-      id={"header-primary"}
-      leftAddon={leftAddon}
-      rightAddon={rightAddon}
-      navs={navs}
-    />
-  );
+    { name: 'Beranda', path: '#' },
+    { name: 'Tentang', path: '#about' },
+    { name: 'Layanan', path: '#services' },
+    { name: 'Tim', path: '#team' },
+    { name: 'Kontak', path: '#contact' },
+  ]
+  return <Header id={'header-primary'} leftAddon={leftAddon} rightAddon={rightAddon} navs={navs} />
 }
 
 function Secondary({ leftAddon, rightAddon }) {
-  const uid = Cookies.get("uid");
-  const [user, setUser] = useState("");
+  const uid = Cookies.get('uid')
+  const [user, setUser] = useState('')
   const { get, response } = useFetch(`${import.meta.env.VITE_API_URL}users`, {
     headers: {
-      Authorization: `Bearer ${jsCookie.get("access_token")}`,
+      Authorization: `Bearer ${jsCookie.get('access_token')}`,
     },
-  });
+  })
   async function getUser() {
-    const data = await get(`/q?id=${uid}`);
+    const data = await get(`/q?id=${uid}`)
     if (response.ok) {
-      setUser(data);
+      setUser(data)
     }
   }
 
   useEffect(() => {
-    getUser();
-  }, []);
+    getUser()
+  }, [])
   const navs = [
-    { name: "Beranda", path: "/dashboard" },
-    { name: "Artikel", path: "/artikel" },
-    { name: "Forum", path: "/forum" },
+    { name: 'Beranda', path: '/dashboard' },
+    { name: 'Artikel', path: '/artikel' },
+    { name: 'Forum', path: '/forum' },
     {
-      name: "Konsultasi",
-      path: user.role === "admin" ? "/konsultasi/admin" : "/konsultasi",
+      name: 'Konsultasi',
+      path: user.role === 'admin' ? '/konsultasi/admin' : '/konsultasi',
     },
-  ];
-  return (
-    <Header
-      id={"header-secondary"}
-      leftAddon={leftAddon}
-      rightAddon={rightAddon}
-      navs={navs}
-    />
-  );
+  ]
+  return <Header id={'header-secondary'} leftAddon={leftAddon} rightAddon={rightAddon} navs={navs} />
 }
 
 function Brand({ v = 2 }) {
@@ -92,7 +94,7 @@ function Brand({ v = 2 }) {
         <h1 className="brand-name">adanest</h1>
       </div>
     </Link>
-  );
+  )
 }
 
 function AuthButton() {
@@ -105,31 +107,31 @@ function AuthButton() {
         <button className="btn btn-outline btn-sm">Buat akun</button>
       </Link>
     </>
-  );
+  )
 }
 
 function NotificationIcon() {
-  return <div className="notification-icon"></div>;
+  return <div className="notification-icon"></div>
 }
 
 function UserProfileBtn() {
-  const uid = Cookies.get("uid");
-  const [user, setUser] = useState("");
+  const uid = Cookies.get('uid')
+  const [user, setUser] = useState('')
   const { get, response } = useFetch(`${import.meta.env.VITE_API_URL}users`, {
     headers: {
-      Authorization: `Bearer ${jsCookie.get("access_token")}`,
+      Authorization: `Bearer ${jsCookie.get('access_token')}`,
     },
-  });
+  })
   async function getUser() {
-    const data = await get(`/q?id=${uid}`);
+    const data = await get(`/q?id=${uid}`)
     if (response.ok) {
-      setUser(data);
+      setUser(data)
     }
   }
 
   useEffect(() => {
-    getUser();
-  }, []);
+    getUser()
+  }, [])
   return (
     <div className="user-profile-btn">
       <img src={user.avatar} alt="user" width={56} height={56} />
@@ -141,7 +143,7 @@ function UserProfileBtn() {
         <Link to="/">Keluar</Link>
       </div>
     </div>
-  );
+  )
 }
 
 export default {
@@ -151,4 +153,4 @@ export default {
   AuthButton,
   NotificationIcon,
   UserProfileBtn,
-};
+}
--- src/pages/homepage/index.jsx
+++ src/pages/homepage/index.jsx
@@ -1,27 +1,24 @@
-import "./style.css";
-import Header from "../../components/shared/header";
-import Container from "../../components/shared/container";
-import Hero from "../../components/homepage/hero";
-import About from "../../components/homepage/about";
-import Why from "../../components/homepage/why";
-import Services from "../../components/homepage/services";
-import Team from "../../components/homepage/team";
-import Contact from "../../components/homepage/contact";
-import Footer from "../../components/shared/footer";
-import { useEffect } from "react";
+import { useEffect } from 'react'
+import About from '../../components/homepage/about'
+import Contact from '../../components/homepage/contact'
+import Hero from '../../components/homepage/hero'
+import Services from '../../components/homepage/services'
+import Team from '../../components/homepage/team'
+import Why from '../../components/homepage/why'
+import Container from '../../components/shared/container'
+import Footer from '../../components/shared/footer'
+import Header from '../../components/shared/header'
+import './style.css'
 
 function Homepage() {
   useEffect(() => {
-    document.title = "Adanest | Homepage";
-  }, []);
+    document.title = 'Adanest | Homepage'
+  }, [])
   return (
-    <>
+    <div id="homepage">
       <div className="hero-bg-yellow">
         <Container>
-          <Header.Primary
-            leftAddon={<Header.Brand v={1} />}
-            rightAddon={<Header.AuthButton />}
-          />
+          <Header.Primary leftAddon={<Header.Brand v={1} />} rightAddon={<Header.AuthButton />} />
           <Hero />
         </Container>
       </div>
@@ -35,7 +32,7 @@ function Homepage() {
       <Footer.FooterContainer>
         <Footer.FooterChild />
       </Footer.FooterContainer>
-    </>
-  );
+    </div>
+  )
 }
-export default Homepage;
+export default Homepage
