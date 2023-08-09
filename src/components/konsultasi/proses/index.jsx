6572b5f3239fafb44f435a971e07d9802dc3fbd3
jrdrwn <jordirwn@gmail.com>
2023-08-09 17:07:48 +0700

    add responsive for konsultasi

--- src/components/konsultasi/proses/index.jsx
+++ src/components/konsultasi/proses/index.jsx
@@ -1,17 +1,17 @@
-import Cookies from "js-cookie";
-import "./style.css";
-import { AiFillCloseCircle, AiOutlineSend } from "react-icons/ai";
-import { Link } from "react-router-dom";
-import { useRef, useEffect, useState } from "react";
-import { useFetch } from "use-http";
-import brand from "./brand-2.svg";
+import Cookies from 'js-cookie'
+import { useEffect, useRef, useState } from 'react'
+import { AiFillCloseCircle, AiOutlineSend } from 'react-icons/ai'
+import { Link } from 'react-router-dom'
+import { useFetch } from 'use-http'
+import brand from './brand-2.svg'
+import './style.css'
 
 function Container({ children }) {
-  return <div className="konsultasi-container">{children}</div>;
+  return <div className="konsultasi-container">{children}</div>
 }
 
 function SideBar({ users, setUser, setMessages }) {
-  const [q, setQ] = useState("");
+  const [q, setQ] = useState('')
 
   return (
     <div className="konsultasi-left">
@@ -22,25 +22,22 @@ function SideBar({ users, setUser, setMessages }) {
             placeholder=" "
             id="konsultasi-user-search"
             onChange={(e) => {
-              setQ(e.target.value);
+              setQ(e.target.value)
             }}
           />
           <label htmlFor="konsultasi-user-search">Cari pengguna...</label>
         </div>
         <div className="konsultasi-users">
           {users
-            .filter((user) =>
-              user.user.username.toLowerCase().includes(q.toLowerCase())
-            )
+            .filter((user) => user.user.username.toLowerCase().includes(q.toLowerCase()))
             .map((user) => (
               <div
                 onClick={() => {
-                  setUser(user.user);
-                  setMessages(user.messages);
+                  setUser(user.user)
+                  setMessages(user.messages)
                 }}
                 className="konsultasi-user"
-                key={user.user._id}
-              >
+                key={user.user._id}>
                 <div className="konsultasi-avatar">
                   <img src={user.user.avatar} alt="" />
                 </div>
@@ -48,31 +45,23 @@ function SideBar({ users, setUser, setMessages }) {
                   <h1>{user.user.username}</h1>
                   <p>{user.messages[user.messages.length - 1].message}</p>
                 </div>
-                {user.unread > 0 && (
-                  <div className="konsultasi-count-new-message">
-                    {user.unread}
-                  </div>
-                )}
+                {user.unread > 0 && <div className="konsultasi-count-new-message">{user.unread}</div>}
               </div>
             ))}
         </div>
       </div>
     </div>
-  );
+  )
 }
 
 function Conversation({ user, messages, sendMessage, closePath }) {
   return (
     <div className="konsultasi-center">
-      <ConversationHeader
-        avatar={user.avatar || brand}
-        username={user.username || "AdaChat"}
-        to={closePath}
-      />
+      <ConversationHeader avatar={user.avatar || brand} username={user.username || 'AdaChat'} to={closePath} />
       <ConversationBody messages={messages} />
       <ConversationFooter sendMessage={sendMessage} />
     </div>
-  );
+  )
 }
 
 function ConversationHeader({ avatar, username, to }) {
@@ -91,59 +80,59 @@ function ConversationHeader({ avatar, username, to }) {
         </div>
       </Link>
     </div>
-  );
+  )
 }
 
 function ConversationBody({ messages }) {
-  const me = JSON.parse(Cookies.get("me"));
+  const me = JSON.parse(Cookies.get('me'))
   const { put } = useFetch(`${import.meta.env.VITE_API_URL}chat`, {
-    cachePolicy: "no-cache",
+    cachePolicy: 'no-cache',
     headers: {
-      Authorization: `Bearer ${Cookies.get("access_token")}`,
+      Authorization: `Bearer ${Cookies.get('access_token')}`,
     },
-  });
-  const ref = useRef(null);
-  const [sh, setSh] = useState(0);
+  })
+  const ref = useRef(null)
+  const [sh, setSh] = useState(0)
 
   const Scroll = () => {
-    const { scrollHeight } = ref.current;
-    setSh(scrollHeight);
+    const { scrollHeight } = ref.current
+    setSh(scrollHeight)
     if (scrollHeight > sh) {
-      ref.current?.scrollTo(0, scrollHeight);
+      ref.current?.scrollTo(0, scrollHeight)
     }
-  };
+  }
 
   useEffect(() => {
     messages?.forEach((msg) => {
       if (msg.state == 0) {
         if (msg.sender._id !== me.id) {
-          put("/read", {
+          put('/read', {
             messageId: msg._id,
-          });
+          })
         }
       }
-    });
-    Scroll();
-  }, [messages]);
+    })
+    Scroll()
+  }, [messages])
   return (
     <div className="konsultasi-body" ref={ref}>
       {messages?.map((message) => (
         <div data-from-me={message.sender._id === me.id} key={message._id}>
           {message.message}
           <span>
-            {new Date(message.createdAt).toLocaleTimeString("id-ID", {
-              hour: "2-digit",
-              minute: "2-digit",
+            {new Date(message.createdAt).toLocaleTimeString('id-ID', {
+              hour: '2-digit',
+              minute: '2-digit',
             })}
           </span>
         </div>
       ))}
     </div>
-  );
+  )
 }
 
 function ConversationFooter({ sendMessage }) {
-  const ref = useRef(null);
+  const ref = useRef(null)
   return (
     <div className="konsultasi-footer">
       <input ref={ref} type="text" placeholder="Type a message" />
@@ -151,7 +140,7 @@ function ConversationFooter({ sendMessage }) {
         <AiOutlineSend size={32} />
       </button>
     </div>
-  );
+  )
 }
 
-export { Container, SideBar, Conversation };
+export { Container, Conversation, SideBar }
--- src/pages/proses-konsultasi/admin/index.jsx
+++ src/pages/proses-konsultasi/admin/index.jsx
@@ -1,83 +1,98 @@
-import { useEffect, useState } from "react";
-import Cookies from "js-cookie";
-import { useFetch } from "use-http";
-import {
-  Container,
-  Conversation,
-  SideBar,
-} from "../../../components/konsultasi/proses";
+import Cookies from 'js-cookie'
+import { useEffect, useState } from 'react'
+import { MdOutlineSwapHorizontalCircle } from 'react-icons/md'
+import { useFetch } from 'use-http'
+import { Container, Conversation, SideBar } from '../../../components/konsultasi/proses'
 
 function KonsultasiAdmin() {
-  const me = JSON.parse(Cookies.get("me"));
-  const [users, setUsers] = useState([]);
-  const [user, setUser] = useState({});
-  const [messages, setMessages] = useState([]);
+  const me = JSON.parse(Cookies.get('me'))
+  const [users, setUsers] = useState([])
+  const [user, setUser] = useState({})
+  const [messages, setMessages] = useState([])
   const { get, post } = useFetch(`${import.meta.env.VITE_API_URL}chat`, {
-    cachePolicy: "no-cache",
+    cachePolicy: 'no-cache',
     headers: {
-      Authorization: `Bearer ${Cookies.get("access_token")}`,
+      Authorization: `Bearer ${Cookies.get('access_token')}`,
     },
-  });
+  })
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+  const [show, setShow] = useState(false)
 
   const sendMessage = (ref) => {
-    if (user._id === undefined) return;
-    post("/send", { message: ref.current.value, to: user._id }).then((res) => {
+    if (user._id === undefined) return
+    post('/send', { message: ref.current.value, to: user._id }).then((res) => {
       if (res) {
-        getMessages(user);
+        getMessages(user)
       }
-    });
-    ref.current.value = "";
-  };
+    })
+    ref.current.value = ''
+  }
 
   const getMessages = (user) => {
     get().then((res) => {
       const grouped = res.reduce((acc, cur) => {
         if (acc[cur.sender._id] && cur.sender._id !== me.id) {
-          if (cur.state == 0) acc[cur.sender._id].unread++;
-          acc[cur.sender._id].messages.push(cur);
-        } else if (acc[cur.receiver._id] && cur.sender.role === "admin") {
-          acc[cur.receiver._id].messages.push(cur);
+          if (cur.state == 0) acc[cur.sender._id].unread++
+          acc[cur.sender._id].messages.push(cur)
+        } else if (acc[cur.receiver._id] && cur.sender.role === 'admin') {
+          acc[cur.receiver._id].messages.push(cur)
         } else {
           if (cur.sender._id !== me.id) {
             acc[cur.sender._id] = {
               user: cur.sender,
               messages: [cur],
               unread: 0,
-            };
-            if (cur.state == 0) acc[cur.sender._id].unread++;
+            }
+            if (cur.state == 0) acc[cur.sender._id].unread++
           }
         }
-        return acc;
-      }, {});
+        return acc
+      }, {})
       if (user._id) {
-        setUser(grouped[user._id].user);
-        setMessages(grouped[user._id].messages);
+        setUser(grouped[user._id].user)
+        setMessages(grouped[user._id].messages)
       }
-      setUsers(Object.values(grouped));
-    });
-  };
+      setUsers(Object.values(grouped))
+    })
+  }
 
   useEffect(() => {
-    const id = setInterval(getMessages, 1000, user);
+    const id = setInterval(getMessages, 1000, user)
 
     return () => {
-      clearInterval(id);
-    };
-  }, [user]);
+      clearInterval(id)
+    }
+  }, [user])
+
   useEffect(() => {
-    document.title = "Adanest | Admin";
-  }, []);
+    const id = addEventListener('resize', () => {
+      setScreenWidth(window.innerWidth)
+    })
+    return () => {
+      removeEventListener('resize', id)
+    }
+  }, [])
+
+  useEffect(() => {
+    document.title = 'Adanest | Admin'
+  }, [])
   return (
     <Container>
-      <SideBar users={users} setUser={setUser} setMessages={setMessages} />
-      <Conversation
-        user={user}
-        messages={messages}
-        sendMessage={sendMessage}
-        closePath={"/dashboard"}
-      />
+      {screenWidth < 675 && (
+        <div className="konsultasi-swap" onClick={() => setShow(!show)}>
+          <MdOutlineSwapHorizontalCircle size={48} />
+        </div>
+      )}
+      {screenWidth < 675 &&
+        (show ? <SideBar users={users} setUser={setUser} setMessages={setMessages} /> : <Conversation user={user} messages={messages} sendMessage={sendMessage} closePath={'/dashboard'} />)}
+      {screenWidth >= 675 && (
+        <>
+          <SideBar users={users} setUser={setUser} setMessages={setMessages} />
+          <Conversation user={user} messages={messages} sendMessage={sendMessage} closePath={'/dashboard'} />
+        </>
+      )}
     </Container>
-  );
+  )
 }
 
-export default KonsultasiAdmin;
+export default KonsultasiAdmin
